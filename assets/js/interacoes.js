e2934a622c3bc3778bae90225d7f808fbe0f5569
mykoll007 <mykoll.daniel@gmail.com>
2024-09-17 15:47:09 -0300

    first commit

---
+++ assets/js/interacoes.js
@@ -0,0 +1,12 @@
+//Hamburguer
+function clickMenu(){
+  
+    if(itens.style.display == 'block' && btnIniciar.style.display == 'block'){
+        itens.style.display = 'none'
+        btnIniciar.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+        btnIniciar.style.display = 'block'
+    }
+
+}
