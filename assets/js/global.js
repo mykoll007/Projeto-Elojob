e78745c8ee81e02919f95a08b9f581a11d0df49d
mykoll007 <mykoll.daniel@gmail.com>
2024-10-01 17:21:23 -0300

    corrigindo header e footers para todas pagina

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,11 +1,11 @@
 //Hamburguer
 function clickMenu() {
-  if (itens.style.display == "block" && btnIniciar.style.display == "block") {
+  if (itens.style.display == "block" && btnIniciar.style.display == "flex") {
     itens.style.display = "none";
     btnIniciar.style.display = "none";
   } else {
     itens.style.display = "block";
-    btnIniciar.style.display = "block";
+    btnIniciar.style.display = "flex";
   }
 }
 
